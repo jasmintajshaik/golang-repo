29ed65895535ed5a80943e68cade646636a190e1
jasmintajshaik <shaikjasmintaj26@gmail.com>
2023-02-18 19:44:01 +0530

    Added README file and first go program

---
+++ hello-world/go.mod
@@ -0,0 +1,3 @@
+module github.com/jasmintajshaik/golang-repo/hello-world
+
+go 1.19
---
+++ hello-world/hello.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hi, Nice to meet you. I am getting started with golang.")
+}
